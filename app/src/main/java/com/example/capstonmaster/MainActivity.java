e120897aa0a6a38ca516b2c9b4727a67187e2dc3
taehi <dksxoghl8@gmail.com>
2019-10-15 17:58:09 +0900

    드로어 상단탭수정, api통신으로 게시판카테고리,내용 불러오기 추가

--- app/src/main/java/com/example/capstonmaster/MainActivity.java
+++ app/src/main/java/com/example/capstonmaster/MainActivity.java
@@ -1,172 +1,246 @@
 package com.example.capstonmaster;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 import android.widget.Toast;
 
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.navigation.NavController;
-
-import androidx.navigation.NavDestination;
-
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import androidx.viewpager.widget.ViewPager;
 
-import com.example.capstonmaster.board.free_board.FreeListViewAdapter;
-import com.example.capstonmaster.dto.List_item;
+import com.example.capstonmaster.board.free_board.Freefragment;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-  private AppBarConfiguration mAppBarConfiguration;
-  private ViewPager viewPager;
-  private SectionsPageAdapter sectionsPageAdapter;
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    this.InitializeLayout();
+    private AppBarConfiguration mAppBarConfiguration;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    private SectionsPageAdapter sectionsPageAdapter;
+    SharedPreferences sf;
+    String userToken;
 
-  }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.appbar_action, menu);
-    return true;
-  }
+    TabLayout.TabLayoutOnPageChangeListener tabLayoutOnPageChangeListener;
+    TabLayout.OnTabSelectedListener onTabSelectedListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case R.id.action_search:
-        Toast.makeText(getApplicationContext(), " 찾기", Toast.LENGTH_SHORT).show();
-//        FreeListViewAdapter adapter;
-//        ArrayList<List_item> list_itemArrayList=new ArrayList<>();
-//        for(int i=0;i<list_itemArrayList.size();i++){
-//          if(list_itemArrayList.get(i).getNickname().equals("보라돌이")){
-//            list_itemArrayList.add(list_itemArrayList.get(i));
-//          }
-//        }
-//        ListView listView;
-//        listView=findViewById(R.id.free_list);
-//        adapter= new FreeListViewAdapter(getApplicationContext(),list_itemArrayList);
-//        listView.setAdapter(adapter);
-        return true;
-      case R.id.action_settings:
-        Toast.makeText(getApplicationContext(), " 설정", Toast.LENGTH_SHORT).show();
-        return true;
-      default:
-        return super.onOptionsItemSelected(item);
+        sf = getSharedPreferences("pref", MODE_PRIVATE);
+        userToken = sf.getString("userToken", "");
+        System.out.println(userToken + "토큰 있음 ㅇㅇ");
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        getBoardList();
+
+        this.InitializeLayout();
     }
-  }
 
-  public void InitializeLayout() {
-    //toolBar를 통해 App Bar 생성
-    Toolbar toolbar = findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_action, menu);
+        return true;
+    }
 
-    sectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
-    viewPager=findViewById(R.id.viewpager);
-    viewPager.setAdapter(sectionsPageAdapter);
-    TabLayout tabLayout = findViewById(R.id.tabs);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                Toast.makeText(getApplicationContext(), " 찾기", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(), " 설정", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void InitializeLayout() {
+
+        //toolBar를 통해 App Bar 생성
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.parseColor("#B0BEC5"));
+//    toolbar.setBackgroundColor(Color.parseColor("#ffffff"));
+
+        sectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(sectionsPageAdapter);
+        tabLayout = findViewById(R.id.tabs);
 //    tabLayout.setupWithViewPager(viewPager);
-    viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-    tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager){
-      @Override
-      public void onTabSelected(TabLayout.Tab tab) {
-        // TODO : tab의 상태가 선택 상태로 변경.
-      }
 
-      @Override
-      public void onTabUnselected(TabLayout.Tab tab) {
-        // TODO : tab의 상태가 선택되지 않음으로 변경.
-      }
+//        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
 
-      @Override
-      public void onTabReselected(TabLayout.Tab tab) {
-        // TODO : 이미 선택된 tab이 다시
-      }
-    });
-
-
-    //App Bar의 좌측 영영에 Drawer를 Open 하기 위한 Incon 추가
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
-
-    DrawerLayout drawLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        tabLayoutOnPageChangeListener = new TabLayout.TabLayoutOnPageChangeListener(tabLayout);
 
 
-    ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
-      this,
-      drawLayout,
-      toolbar,
-      R.string.app_name,
-      R.string.app_name
-    );
 
-    drawLayout.addDrawerListener(actionBarDrawerToggle);
+        //App Bar의 좌측 영영에 Drawer를 Open 하기 위한 Incon 추가
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//    getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
 
-    //프래그먼트 장착
-    mAppBarConfiguration = new AppBarConfiguration.Builder(
-      R.id.login,R.id.free,R.id.notice)
-      .setDrawerLayout(drawLayout)
-      .build();
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-
-//         ActionBar에도 Top-Level-Destination 설정 하면,
-//Top-Level-Destincation이 아니라면 Actionbar에 뒤로 가기[<-] 가 표현된다
-    NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-    NavigationUI.setupWithNavController(navigationView, navController);
-
-    navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
-
-      public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
-        Toast.makeText(MainActivity.this, destination.getLabel(),Toast.LENGTH_LONG).show();
-      }
-    });
+        DrawerLayout drawLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 
 
-               /* navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                        switch (menuItem.getItemId())
-                        {
-                            case R.id.club_intro:
-                                Toast.makeText(getApplicationContext(), "intro 2", Toast.LENGTH_SHORT).show();
-                            case R.id.pro_intro:
-                                Toast.makeText(getApplicationContext(), "intro 3", Toast.LENGTH_SHORT).show();
-                        }
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawLayout,
+                toolbar,
+                R.string.app_name,
+                R.string.app_name
+        );
+
+        drawLayout.addDrawerListener(actionBarDrawerToggle);
+
+        actionBarDrawerToggle.syncState();
+
+//    //프래그먼트 장착
+//    mAppBarConfiguration = new AppBarConfiguration.Builder(
+//      R.id.login,R.id.free,R.id.notice)
+//      .setDrawerLayout(drawLayout)
+//      .build();
+//    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//
+////         ActionBar에도 Top-Level-Destination 설정 하면,
+////Top-Level-Destincation이 아니라면 Actionbar에 뒤로 가기[<-] 가 표현된다
+//    NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//    NavigationUI.setupWithNavController(navigationView, navController);
+
+//    navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+//
+//      public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+//
+//        Toast.makeText(MainActivity.this,destination.getLabel(),Toast.LENGTH_LONG).show();
+//      }
+//    });
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.free:
+                        Toast.makeText(getApplicationContext(), "자유", Toast.LENGTH_SHORT).show();
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.notice:
+                        Toast.makeText(getApplicationContext(), "공지", Toast.LENGTH_SHORT).show();
+                        viewPager.setCurrentItem(2);
+                        break;
+                    case R.id.mento_menti:
+                        Toast.makeText(getApplicationContext(), "멘토", Toast.LENGTH_SHORT).show();
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.viewpager,new Loginfragment()).commit();
+                        break;
+                }
+//                getSupportActionBar().setTitle();
                 DrawerLayout drawer = findViewById(R.id.drawer_layout);
                 drawer.closeDrawer(GravityCompat.START);
                 return true;
             }
-        });*/
-  }
-
-  @Override
-  public void onBackPressed() {
-    DrawerLayout drawer = findViewById(R.id.drawer_layout);
-    if (drawer.isDrawerOpen(GravityCompat.START)) {
-      drawer.closeDrawer(GravityCompat.START);
-    } else {
-      super.onBackPressed();
+        });
     }
-  }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        SharedPreferences.Editor editor=sf.edit();
+        editor.remove("pref");
+        editor.commit();
+        System.out.println("토큰지움");
+        super.onDestroy();
+    }
+    public void getBoardList(){
+
+        OkHttpClient client = new OkHttpClient();
+
+        final Request request = new Request.Builder()
+                .header(getString(R.string.Authorization), "Bearer "+userToken)
+                .url(getString(R.string.ip)+"/api/article/category")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println(e);
+                System.out.println("실패");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    JSONArray jsonArray = new JSONArray(response.body().string());
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonobject = jsonArray.getJSONObject(i);
+                        String id   = jsonobject.getString("id");
+                        String category = jsonobject.getString("name");
+                        switch (id){
+                            case "1":
+                                break;
+                            case "2":
+                                Freefragment.id = id;
+                                break;
+                            case "3":
+                                break;
+                            case "4":
+                                break;
+                        }
+                        System.out.println(id+" "+ category);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        });
+    }
 }
--- app/src/main/java/com/example/capstonmaster/board/free_board/Freefragment.java
+++ app/src/main/java/com/example/capstonmaster/board/free_board/Freefragment.java
@@ -1,6 +1,10 @@
 package com.example.capstonmaster.board.free_board;
 
+import android.content.Context;
+import android.content.Entity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -14,20 +18,43 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 
+import com.example.capstonmaster.MainActivity;
 import com.example.capstonmaster.dto.List_item;
 import com.example.capstonmaster.R;
+import com.google.android.material.tabs.TabItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class Freefragment extends Fragment {
-//    private FreeViewModel freeViewModel;
-ListView listView;
+    ListView listView;
     FreeListViewAdapter adapter;
     ArrayList<List_item> list_itemArrayList;
+    SharedPreferences sf;
+    String userToken;
+
+    public static String id;
+    public static String category;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         final View root = inflater.inflate(R.layout.fragment_free, container, false);
+
+        sf= root.getContext().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        userToken=sf.getString("userToken","");
+        getBoard();
+
         listView=root.findViewById(R.id.free_list);
         list_itemArrayList=new ArrayList<>();
         list_itemArrayList.add(new List_item("보라돌이","안녕","10월 9일","반가워요 하하하ㅏㅋㅋㅋㅋㅋㅋㅋㅋㅠㅠㅠㅠㅠㄴㅠㅠㅠㅠ"));
@@ -60,16 +87,45 @@ ListView listView;
                 startActivity(intent);
             }
         });
-//        freeViewModel =
-//                ViewModelProviders.of(this).get(FreeViewModel.class);
-//        final TextView textView = root.findViewById(R.id.freeboard);
-//        freeViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+
         return root;
+    }
+    public void getBoard(){
+        System.out.println(id + "카테고리 아이디값~~~~~~~~~~~~~~~~~~~~~");
+        OkHttpClient client = new OkHttpClient();
+
+        final Request request = new Request.Builder()
+                .header(getString(R.string.Authorization), "Bearer "+userToken)
+                .url(getString(R.string.ip)+"/api/article/"+ id)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println(e);
+                System.out.println("실패");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+//                    JSONArray jsonArray = new JSONArray(response.body().string());
+//                    for (int i = 0; i < jsonArray.length(); i++) {
+//                        JSONObject jsonobject = jsonArray.getJSONObject(i);
+//                        String contents = jsonobject.getString("contents");
+//                        String title = jsonobject.getString("title");
+//                        System.out.println(title + " " + contents);
+//                    }
+                    JSONObject jsonobject =new JSONObject(response.body().string());
+                    String m = jsonobject.getString("content");
+                    System.out.println(m+" 뭐라도찍어봐");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        });
     }
 
 }
